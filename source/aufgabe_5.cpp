8e000041cca152e09509b0c8b58dfc2451493eb2
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-05-22 20:04:23 +0200

    anfangen mit aufgabe 6

--- source/aufgabe_5.cpp
+++ source/aufgabe_5.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<map>
 
 int main()
 {
+std::map < std::string , int > matrikelnummern ;
+// Hinzufuegen von vielen Studenten
+matrikelnummern ["Max Mustermann"] = 12345;
+matrikelnummern ["Erika Mustermann"] = 23523;
+// ...
+//exmatrikulation (matrikelnummer ["Fred Fuchs"]);
+//auto& it = matrikelnummern.find("Erika Mustermann");
+//matrikelnummern.erase(it);
+
+matrikelnummern.erase("Erika Mustermann");
     return 0;
 }
