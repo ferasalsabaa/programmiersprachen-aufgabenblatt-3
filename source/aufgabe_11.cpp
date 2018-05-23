0c6f5b530152d404589d443c0eb627a14cd5d8b9
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-05-23 17:52:52 +0200

    aufgabe 11

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -4,23 +4,36 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-
-
+std::vector<unsigned int> numbers(100); 
+bool is_multiple_of_3(int i)
+{
+  return (i % 3) == 0;  
+}
+bool is_not_multiple_of_3(int i)
+{
+  return (i % 3) != 0;  
+}
 
 TEST_CASE ("filter alle vielfache von drei","[ erase ]")
 {
     
 // ihre Loesung :
 // ...
-//REQUIRE (std::all_of(v.begin() ,v.end() , is_multiple_of_3 ));
+REQUIRE (std::all_of(numbers.begin() ,numbers.end() , is_multiple_of_3 ));
 }
 int main ( int argc , char * argv [])
 {
-     vector<unsigned int> numbers(100);   
+       
      for( auto& i : numbers)
       {
         i = rand() % 100 +1;
-      // cout << i << ", ";
+      }
+
+  numbers.erase( std::remove_if(numbers.begin(), numbers.end(), is_not_multiple_of_3), numbers.end() );
+ 
+ for( auto& i : numbers)
+      {
+      std::cout << i << ", ";
       }
 return Catch :: Session (). run ( argc , argv );
 }
