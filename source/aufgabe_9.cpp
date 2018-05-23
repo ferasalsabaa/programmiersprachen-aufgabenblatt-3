3cfe7a521634277fff1011257c15c9a524007ba3
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-05-23 16:17:27 +0200

    aufgabe 9

--- source/aufgabe_9.cpp
+++ source/aufgabe_9.cpp
@@ -14,18 +14,18 @@ int main()
     }
 
     std::copy(std::cbegin(v_0),std::cend(v_0),
-    std::ostream_iterator<int>(std::cout,"\n"));
+    std::ostream_iterator<int>(std::cout,"\n"));    //cout 10 Numbers random
 
     std::list<int> l_0(std::cbegin(v_0), std::cend(v_0));
-    std::list <int> l_1 ( std::cbegin(l_0),std::cend(l_0));
-    std::reverse ( std::begin(l_1),std::end(l_1));
-    std::copy ( std::cbegin(l_1),std::cend(l_1) ,
+    std::list<int> l_1( std::cbegin(l_0),std::cend(l_0));
+    std::reverse( std::begin(l_1),std::end(l_1));           //swap
+    std::copy(std::cbegin(l_1),std::cend(l_1) ,
     std::ostream_iterator<int>(std::cout,"\n"));
 
-l_1.sort();
+l_1.sort();                                             //sort
 std::copy (l_1.cbegin(),l_1.cend(),
 std::ostream_iterator<int>(std::cout,"\n"));
-std::generate(std::begin(v_0),std::end (v_0),std::rand);
+std::generate(std::begin(v_0),std::end(v_0),std::rand);
 std::copy(v_0.crbegin(),v_0.crend(),
 std::ostream_iterator<int>(std::cout,"\n"));
 
